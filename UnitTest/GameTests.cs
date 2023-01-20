5a9063ce62484d1a9cfe3ec269670f02d6e81e7a
Koleszár Tamás <koleszar.tamas@hmei.hu>
2023-01-20 08:46:06 +0100

    Gamecontroller konstruktor teszt

---
+++ UnitTest/GameTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using szamkitjat;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class GameTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException),  "Constructor null paraméter exception test ok.")]
+        public void GameContructorNullReferenceTest()
+        {
+            _ = new Game(null);
+        }
+    }
+}
