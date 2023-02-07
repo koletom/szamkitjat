f5045cc755fd60b302d628dfa1cb8b6fdbd64985
Koleszár Tamás <koleszar.tamas@hmei.hu>
2023-02-07 09:26:55 +0100

    winnercheck

--- szamkitjatservices/TicTacToeService.cs
+++ szamkitjatservices/TicTacToeService.cs
@@ -32,7 +32,89 @@ namespace szamkitjatservices
 
         private byte WinnerChecking()
         {
-            return 0;
+            int row = 0, col = 0, i = 0;
+            bool areFull = false;
+
+            for (byte gamer = 1; gamer <= 2; gamer++)
+            {
+
+                for (row = 0; row < Table.GetLength(0); row++)                       
+                {
+                    for (col = 0; col < Table.GetLength(1); col++)           
+                    {
+                        if(!areFull && Table[row,col]==0 )
+                        {
+                            areFull = true;
+                            continue;
+                        };
+
+                        while (Table[row,col] == gamer)      
+                        {
+                            col++;
+                            i++;
+                            if (i == 3)
+                            {
+                                return gamer;
+                            }
+                        }
+                        i = 0;
+                    }
+                }
+
+                for (col = 0; col < Table.GetLength(1); col++)                       
+                {
+                    for (row = 0; row < Table.GetLength(0); row++)
+                    {
+                        while (Table[row,col] == gamer)
+                        {
+                            row++;
+                            i++;
+                            if (i == 3)
+                            {
+                                return gamer;
+                            }
+                        }
+                        i = 0;
+                    }
+                }
+
+                for (col = 0; col < Table.GetLength(1); col++)             
+                {
+                    for (row = 0; row < Table.GetLength(0); row++)
+                    {
+                        while (Table[row,col] == gamer)
+                        {
+                            row++;
+                            col++;
+                            i++;
+                            if (i == 3)
+                            {
+                                return gamer;
+                            }
+                        }
+                        i = 0;
+                    }
+                }
+                
+                for (col = Table.GetLength(1) - 1; col >= 0 ; col--) 
+                {                                                   
+                    for (row = 0; row < Table.GetLength(0); row++)    
+                    {
+                        while (Table[row,col] == gamer) 
+                        {
+                            row++;                                  
+                            col--;                                  
+                            i++;                                    
+                            if (i == 3)                             
+                            {
+                                return gamer;                      
+                            }                                      
+                        }                                          
+                        i = 0;                                     
+                    }                                              
+                }
+            }
+            return !areFull ? (byte)0:(byte)1;
         }
     }
 }
